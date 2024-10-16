d40a97f78db735e645d663b1f1406000ea8fc4cf
leodoin <leodoin@gmail.com>
2024-10-16 11:13:20 +0200

    feat(web): add AboutPage and ContactPage components
    - Added AboutPage component to display information about the company.
    - Added ContactPage component to provide contact information.
    - Added componets folder

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './globals.css';
+
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Translation App',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,9 +3,11 @@
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-import TranslationInput from "./translation-input"; // Import the new component
+import TranslationInput from "./components/translation-input/translation-input"; // Import the new component
 
-export default function Home() {
+
+
+export default function Page() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
