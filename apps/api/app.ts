188d4af14854d8029cd112d8cf9196fa96be7c68
leodoin <leodoin@gmail.com>
2024-10-17 22:30:32 +0200

    Refactor API app configuration and server setup to add CORS support

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { translationApi } from './src/translation-api';
 import { helloWorldApi } from './src/hello-world-api';
+import cors from 'cors';
 
 const app = express()
 
 const port = 8080
 
 app.use(express.json())
+
+app.use(cors())
 
 
 app.listen(port, () => {
