d9cdcb2a823a005ae32032ff69e00d52d9a111d3
leodoin <leodoin@gmail.com>
2024-10-17 18:26:04 +0200

    testing api deploy

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -3,23 +3,25 @@ import cors from 'cors';
 import { translationApi } from './src/translation-api';
 import { helloWorldApi } from './src/hello-world-api';
 
-const env = process.env.NODE_ENV || "dev";
-const config = require(`./config.${env}.json`);
+// const env = process.env.NODE_ENV || "dev";
+// const config = require(`./config.${env}.json`);
 
-console.log('NODE_ENV:', process.env.NODE_ENV);
+// console.log('NODE_ENV:', process.env.NODE_ENV);
 
-const { port, allowCors } = config
+// const { port, allowCors } = config
 
 const app = express()
 
-app.use(cors(allowCors))
-console.log(`CORS enabled for ${allowCors.origin}`)
+app.use(cors({
+  "origin":["https://translation-app-web.vercel.app/"]
+}))
+// console.log(`CORS enabled for ${allowCors.origin}`)
 
 app.use(express.json())
 
 
-app.listen(port, () => {
-  console.log(`app listening on http://localhost:${port}`)
+app.listen(8080, () => {
+  // console.log(`app listening on http://localhost:${port}`)
 })
 
 app.get('/', helloWorldApi)
