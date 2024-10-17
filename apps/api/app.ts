b2104e345459d0b4bb65cbef6311fe45783febf0
leodoin <leodoin@gmail.com>
2024-10-17 22:09:20 +0200

    Refactor API app configuration and server setup for CORS support

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -1,15 +1,10 @@
 import express from 'express';
-import cors from 'cors';
 import { translationApi } from './src/translation-api';
 import { helloWorldApi } from './src/hello-world-api';
 
 const app = express()
 
 const port = 8080
-
-app.use(cors({
-  "origin":['https://translation-app-web.vercel.app/','http://localhost:3000']
-}))
 
 app.use(express.json())
 
