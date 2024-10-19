5c7d3627f6cfb02c2bc92eedf7cfbd0a74c30d35
leodoin <leodoin@gmail.com>
2024-10-19 06:22:14 +0200

    upd url

--- apps/web/src/app/services/chatApi.ts
+++ apps/web/src/app/services/chatApi.ts
@@ -34,7 +34,9 @@ type ConnectionParams = {
 }
 
 //const socket = io(`http://localhost:3001}`);
-const socket = io("https://translations-service-332538335160.europe-west4.run.app");
+const socket = io("https://translations-service-332538335160.europe-west4.run.app", {
+  transports: ["websocket"]
+});
 let isConnected = false;
 
 
