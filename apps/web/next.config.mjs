83702b13d9efc82e5eb446e6fe0d87e8855d77ee
leodoin <leodoin@gmail.com>
2024-10-19 05:50:15 +0200

    api uri config upd

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    apiUri: process.env.NEXT_PUBLIC_API_URI,
+    apiUri: process.env.NEXT_PUBLIC_API_BASE_URI,
 };
 
 export default nextConfig;
